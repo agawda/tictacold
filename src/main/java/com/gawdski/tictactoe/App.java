45e68f5eeb281fa15f33298b5c916c0ad5a7d7c2
Anna Gawda <a.m.gawda@gmail.com>
2017-06-28 12:50:36 +0200

    Get board dimensions from the user.

--- src/main/java/com/gawdski/tictactoe/App.java
+++ src/main/java/com/gawdski/tictactoe/App.java
@@ -6,7 +6,23 @@ public class App {
     public static void main( String[] args ) {
         System.out.println("Please choose the starting symbol (X or O): ");
         Scanner scanner = new Scanner(System.in);
+
         String symbol = scanner.nextLine();
         System.out.println(symbol);
+
+        int x;
+        int y;
+
+        System.out.println("Please enter board dimensions: ");
+        System.out.println("x: ");
+        x = Integer.parseInt(scanner.nextLine());
+        System.out.println("y: ");
+        y = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(x + " " + y);
+
+//        for(int i = 0; i < 3; i++) {
+//
+//        }
     }
 }
